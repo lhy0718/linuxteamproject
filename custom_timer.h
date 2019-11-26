ab053a0a7903493ac08b2559d8fd87e5f68ef51c
hanyonglee <glhy0718@gmail.com>
2019-11-27 03:54:20 +0900

    sketch

--- custom_timer.h
+++ custom_timer.h
@@ -2,7 +2,8 @@
 #define CUSTOM_TIMER
 #include <linux/time.h>
 
-#define TIMER_START long TIMER; struct timespec start, end, difftime; getnstimeofday(&start);
+#define TIMER_INIT long TIMER; struct timespec start, end, difftime; 
+#define TIMER_START getnstimeofday(&start);
 #define TIMER_END getnstimeofday(&end); timespec_diff(&start, &end, &difftime); TIMER = difftime.tv_nsec;
 
 void timespec_diff(struct timespec*, struct timespec*, struct timespec*);
--- hanyong.c
+++ hanyong.c
@@ -7,13 +7,45 @@
 #define FALSE 0
 #define TRUE 1
 
+TIMER_INIT;
+
 typedef struct{
 	struct rb_node node;
 	int key;
 	int value;
 }mytype;
 
+typedef struct{
+	struct rb_root *root;
+	mytype *data;
+}rd;
+
+typedef struct{
+	struct rb_root *root;
+	int key;
+}rk;
+
 int rb_insert(struct rb_root *root, mytype *data){
+	struct rb_node **new = &(root->rb_node), *parent = NULL;
+
+	while(*new){
+		mytype *this = container_of(*new, mytype, node);
+		parent = *new;
+		if(this->key > data->key)
+			new = &((*new)->rb_left);
+		else if(this->key < data->key)
+			new = &((*new)->rb_right);
+		else
+			return FALSE;
+	}
+
+	rb_link_node(&data->node, parent, new);
+	rb_insert_color(&data->node, root);
+
+	return TRUE;
+}
+
+int custom_rb_insert(struct rb_root *root, mytype *data){
 	struct rb_node **new = &(root->rb_node), *parent = NULL;
 
 	while(*new){
@@ -48,6 +80,21 @@ mytype *rb_search(struct rb_root *root, int key){
 	return NULL;
 }
 
+mytype *custom_rb_search(struct rb_root *root, int key){
+	struct rb_node *node = root->rb_node;
+
+	while(node){
+		mytype *data = container_of(node, mytype, node);
+		if(data->key > key)
+			node = node->rb_left;
+		else if(data->key < key)
+			node = node->rb_right;
+		else
+			return data;
+	}
+	return NULL;
+}
+
 int rb_delete(struct rb_root *mytree, int key){
 	mytype *data = rb_search(mytree, key);
 	if(data){
@@ -58,10 +105,54 @@ int rb_delete(struct rb_root *mytree, int key){
 	return FALSE;
 }
 
+int custom_rb_delete(struct rb_root *mytree, int key){
+	mytype *data = custom_rb_search(mytree, key);
+	if(data){
+		rb_erase(&data->node, mytree);
+		kfree(data);
+		return TRUE;
+	}
+	return FALSE;
+}
+
+int ith(void *args){
+	struct rb_root *root = ((rd *)args)->root;
+	mytype *data = ((rd *)args)->data;
+	return 0;
+}
+
+int cith(void *args){
+	struct rb_root *root = ((rd *)args)->root;
+	mytype *data = ((rd *)args)->data;
+	return 0;
+}
+
+int sth(void *args){
+	struct rb_root *root = ((rk *)args)->root;
+	int key = ((rk *)args)->key;
+	return 0;
+}
+
+int csth(void *args){
+	struct rb_root *root = ((rk *)args)->root;
+	int key = ((rk *)args)->key;
+	return 0;
+}
+
+int dth(void *args){
+	struct rb_root *root = ((rk *)args)->root;
+	int key = ((rk *)args)->key;
+	return 0;
+}
+
+int cdth(void *args){
+	struct rb_root *root = ((rk *)args)->root;
+	int key = ((rk *)args)->key;
+	return 0;
+}
+
 void test(void){
-	TIMER_START;
-	TIMER_END;
-	printk("timer : %ld\n", TIMER); 
+	struct rb_root control, treatment;
 }
 
 int __init _module_init(void){
